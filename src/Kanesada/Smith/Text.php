b06c90ec3440ff1d6f0b4033a5ad9ba46d6a6ea2
KNJ <developer@konanchuo.yokohama>
2017-09-28 17:01:54 +0900

    Store initial state

--- src/Kanesada/Smith/Text.php
+++ src/Kanesada/Smith/Text.php
@@ -4,10 +4,12 @@ namespace Wazly\Kanesada\Smith;
 
 class Text
 {
+    protected $baseText;
     protected $text;
 
     protected function __construct($text)
     {
+        $this->baseText = $text;
         $this->text = $text;
     }
 
@@ -16,13 +18,56 @@ class Text
         return new static($text);
     }
 
-    public function dumpText(): string
+    /**
+     * Set alternative text.
+     *
+     * @param string $text
+     * @return Text
+     */
+    public function set(string $text): Text
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Rollback the text to initial state.
+     *
+     * @return Text
+     */
+    public function reset(): Text
+    {
+        $this->text = $this->baseText;
+
+        return $this;
+    }
+
+    /**
+     * Output the text and rollback to initial state.
+     *
+     * @return string
+     */
+    public function flush(): string
+    {
+        $text = $this->text;
+        $this->reset();
+
+        return $text;
+    }
+
+    /**
+     * Output the current text.
+     *
+     * @return string
+     */
+    public function dump(): string
     {
         return $this->text;
     }
 
     public function __toString()
     {
-        return $this->dumpText();
+        return $this->dump();
     }
 }
---
+++ tests/Kanesada/Smith/SmithTextTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Wazly\Kanesada\Smith;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmithTextTest extends TestCase
+{
+    const INI_STR = 'Anything one man can imagine, other men can make real.';
+
+    public function setUp()
+    {
+        $this->originalString = self::INI_STR;
+        $this->text = Text::new($this->originalString);
+    }
+
+    public function testDumpOriginalString(): void
+    {
+        $this->assertSame(self::INI_STR, $this->text->dump());
+    }
+}
