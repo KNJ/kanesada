86a8aa5e5a0e3a3345adc47c5c04503da1dd6645
KNJ <developer@konanchuo.yokohama>
2017-10-01 01:01:01 +0900

    Complete tests

--- tests/Kanesada/Smith/SmithTextTest.php
+++ tests/Kanesada/Smith/SmithTextTest.php
@@ -9,6 +9,7 @@ use PHPUnit\Framework\TestCase;
 final class SmithTextTest extends TestCase
 {
     const INI_STR = 'Anything one man can imagine, other men can make real.';
+    const REP_STR = 'Done is better than perfect.';
 
     public function setUp()
     {
@@ -16,8 +17,39 @@ final class SmithTextTest extends TestCase
         $this->text = Text::new($this->originalString);
     }
 
-    public function testDumpOriginalString(): void
+    public function testDumpOriginalString()
     {
         $this->assertSame(self::INI_STR, $this->text->dump());
+    }
+
+    public function testConvertToString()
+    {
+        $this->assertSame(self::INI_STR, (string)$this->text);
+    }
+
+    public function testReplaceString()
+    {
+        $this->text->set(self::REP_STR);
+        $this->assertSame(self::REP_STR, $this->text->dump());
+
+        return $this->text;
+    }
+
+    /**
+     * @depends clone testReplaceString
+     */
+    public function testResetString(Text $text)
+    {
+        $text->reset();
+        $this->assertSame(self::INI_STR, $text->dump());
+    }
+
+    /**
+     * @depends clone testReplaceString
+     */
+    public function testFlushString(Text $text)
+    {
+        $this->assertSame(self::REP_STR, $text->flush());
+        $this->assertSame(self::INI_STR, $text->dump());
     }
 }
